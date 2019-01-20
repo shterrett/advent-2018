097186cb6a5249a24312c6f81d47e4ce56eb908d
Stuart Terrett <shterrett@gmail.com>
2019-01-20 11:48:41 -0500

    Day 7 pt 2

--- app/Main.hs
+++ app/Main.hs
@@ -5,4 +5,4 @@ import qualified Data.Text as T
 import Day7
 
 main :: IO ()
-main = day7 <$> T.lines <$> Tio.readFile "inputs/day-7" >>= Tio.putStrLn
+main = day7p2 5 60 <$> T.lines <$> Tio.readFile "inputs/day-7" >>= Tio.putStrLn
--- src/Day7.hs
+++ src/Day7.hs
@@ -10,26 +10,49 @@ import Data.HashSet (HashSet)
 import qualified Data.Heap as Heap
 import Data.Heap (MinHeap)
 import Data.Maybe (fromMaybe)
+import Data.List (elemIndex, sort, maximumBy)
 import Control.Arrow ((&&&))
 import Text.Parsec
 
 type Node = Char
 type Edge = (Node, Node)
 type Graph = HashMap Node [Node]
+type Seconds = Int
+type Workers = [Seconds]
+
+data Task = Task { startsAt :: Seconds, node :: Node }
+          deriving (Show)
+instance Eq Task where
+    (==) (Task _ n1) (Task _ n2) = n1 == n2
+instance Ord Task where
+    compare (Task s1 n1) (Task s2 n2) = case compare s1 s2 of
+                                          EQ -> compare n1 n2
+                                          r -> r
 
 data SortState = SortState { graph :: Graph
                            , edgeCount :: HashMap Node Integer
-                           , availableNodes :: MinHeap Node
+                           , availableNodes :: MinHeap Task
                            , reversedSort :: [Node]
+                           , workers :: Workers
+                           , nextStart :: Seconds
+                           , costFn :: Node -> Seconds
                            }
 
-initState :: [Edge] -> SortState
-initState es =
+initState :: Int -> Seconds -> [Edge] -> SortState
+initState wc base es =
     let
       graph = buildGraph es
       edgeCount = buildEdgeCount es
-      sources = Heap.fromList $ findSources graph edgeCount
-    in SortState graph edgeCount sources []
+      sources = findSources graph edgeCount
+      workers = take wc $ repeat 0
+    in SortState { graph = graph
+                 , edgeCount = edgeCount
+                 , availableNodes = Heap.fromList $ fmap (Task 0) sources
+                 , reversedSort = []
+                 , workers = workers
+                 , nextStart = 0
+                 , costFn = taskDuration base
+                 }
 
 findSources :: Graph -> HashMap Node Integer -> [Node]
 findSources g ns = Set.toList $ Set.difference (keySet g) (keySet ns)
@@ -51,27 +74,40 @@ buildEdgeCount = foldr incNode Map.empty
   where incNode :: Edge -> HashMap Node Integer -> HashMap Node Integer
         incNode (_, t) g = Map.insertWith (+) t 1 g
 
-topologicalSort :: [Edge] -> [Node]
-topologicalSort es = let s = initState es in
-  reverse . reversedSort $ doSort s
+topologicalSort :: SortState -> [Node]
+topologicalSort s = reverse . reversedSort $ doSort s
 
 doSort :: SortState -> SortState
 doSort s | Heap.isEmpty (availableNodes s) = s
          | otherwise = fromMaybe s $ (doSort .
                                       updateHeap .
                                       pruneEdges .
+                                      updateWorkers .
                                       pushNode s
                                      ) <$>
                                      (Heap.view (availableNodes s))
 
-pushNode :: SortState -> (Node, MinHeap Node) -> (Node, SortState)
-pushNode s (n, h) = (n, s { reversedSort = n:(reversedSort s)
+pushNode :: SortState -> (Task, MinHeap Task) -> (Task, SortState)
+pushNode s (t, h) = (t, s { reversedSort = (node t):(reversedSort s)
                           , availableNodes = h
                           })
 
-pruneEdges :: (Node, SortState) -> SortState
-pruneEdges (n, s) =
-    let neighbors = Map.lookup n (graph s) in
+updateWorkers :: (Task, SortState) -> (Task, SortState)
+updateWorkers (t, s) =
+    let
+      ws = sort $ workers s
+      taskTime = costFn s $ node t
+      taskStart = max (startsAt t) $ head ws
+    in
+      ( t
+      , s { nextStart = taskTime + taskStart
+          , workers = (taskTime + taskStart) : (tail ws)
+          }
+      )
+
+pruneEdges :: (Task, SortState) -> SortState
+pruneEdges (t, s) =
+    let neighbors = Map.lookup (node t) (graph s) in
       fromMaybe s (decrementEdges s <$> neighbors)
   where decrementEdges s ns = s {
       edgeCount = foldr (Map.adjust (\c -> c - 1)) (edgeCount s) ns
@@ -83,11 +119,26 @@ updateHeap s =
       (sources, rest) = splitEdgeCount (edgeCount s)
     in
       s { edgeCount = rest
-        , availableNodes = foldr Heap.insert (availableNodes s) sources
+        , availableNodes = foldr Heap.insert (availableNodes s) $
+                                             fmap (Task (nextStart s)) sources
         }
   where splitEdgeCount = (Map.keys . Map.filter (== 0)) &&& (Map.filter (/= 0))
 
 day7 :: [T.Text] -> T.Text
 day7 lines = either (T.pack . show) T.pack $
               topologicalSort <$>
+              initState 1 0 <$>
               parseInput lines
+
+taskDuration :: Seconds -> Node -> Seconds
+taskDuration base n = base + (cost n)
+  where cost n = fromMaybe 0 $ (+1) <$> elemIndex n "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+workTasks :: SortState -> Seconds
+workTasks s = maximumBy compare $ workers $ doSort s
+
+day7p2 :: Int -> Seconds -> [T.Text] -> T.Text
+day7p2 wc base lines = either (T.pack . show) (T.pack . show) $
+                        workTasks <$>
+                        initState wc base <$>
+                        parseInput lines
