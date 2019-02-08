4fe186f0e363cbbf3b1d742277ea6008aa52cb78
Stuart Terrett <shterrett@gmail.com>
2019-02-08 12:36:16 -0500

    Day11p2
    It still runs really slowly; I should come back at some point

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,59 +1,119 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 
 module Day11 where
 
 import qualified Data.Text as T
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as Map
-import Data.List (maximumBy)
+import Data.List (foldl')
 import Control.Arrow ((&&&))
 import Control.Monad (foldM)
 import Control.Monad.Writer.Lazy (writer)
 
 type Power = Integer
-type Size = Integer
 type SerialNo = Integer
+type Size = Integer
 type Point = (Integer, Integer)
-type Corner = Point
-type Cells = HashMap Point Integer
+type Cells = HashMap (Integer, Point) Integer
 
-grid :: Size -> Point -> [Point]
-grid size (x, y) = (,) <$> [x..x+(size - 1)] <*> [y..y+(size - 1)]
+data Problem = Problem { serialNo :: Integer
+                       , size :: Integer
+                       }
+                       deriving (Show)
 
-powerRemaining :: SerialNo -> Cells -> Point -> (Cells, Power)
-powerRemaining sn cs p = case Map.lookup p cs of
-                           Just power -> (cs, power)
-                           Nothing -> (cachePower p cs) &&& id $ calculatePower sn p
-  where cachePower p cs = flip (Map.insert p) cs
+data GridResult = GridResult { corner :: Point
+                             , gridSize :: Integer
+                             , power :: Power
+                             }
+                             deriving (Show, Eq)
+
+instance Ord GridResult where
+    compare g1 g2 = compare (power g1) (power g2)
+
+gridPower :: Problem -> Cells -> Point -> (Cells, Power)
+gridPower pb@(Problem sn 1) cells pt = lookupOrCalculate pb cells pt (calculatePower sn)
+gridPower pb@(Problem sn sz) cells pt | odd sz = lookupOrCalculate pb cells pt (recurLeadingEdge pb cells)
+                                      | otherwise = lookupOrCalculate pb cells pt (recurBox pb cells)
+
+lookupOrCalculate :: Problem -> Cells -> Point -> (Point -> Power) -> (Cells, Power)
+lookupOrCalculate (Problem sn sz) cells pt calc = case Map.lookup (sz, pt) cells of
+                                                      Just power -> (cells, power)
+                                                      Nothing -> cachePower sz cells pt $ calc pt
 
 calculatePower :: SerialNo -> Point -> Power
 calculatePower sn (x, y) = hundredsDigit (nonsense sn x y) - 5
   where nonsense sn x y = (((x + 10) * y) + sn) * (x + 10)
         hundredsDigit n = mod (div n 100) 10
 
-gridPower :: SerialNo -> Cells -> [Point] -> (Cells, Power)
-gridPower sn cs ps = foldr (accumulatePower sn) (cs, 0) ps
+cachePower :: Size -> Cells -> Point -> Power -> (Cells, Power)
+cachePower sz cells pt = insertCache (sz, pt) cells &&& id
+  where insertCache p cells = flip (Map.insert p) cells
 
-accumulatePower :: SerialNo -> Point -> (Cells, Power) -> (Cells, Power)
-accumulatePower sn p (cs, power) = (+ power) <$> powerRemaining sn cs p
+recurLeadingEdge :: Problem -> Cells -> Point -> Power
+recurLeadingEdge pb@(Problem sn sz) cells pt = (leadingEdge pb cells pt) +
+                                          (snd $ gridPower (Problem sn (sz - 1))
+                                                           cells
+                                                           pt)
+  where leadingEdge :: Problem -> Cells -> Point -> Power
+        leadingEdge (Problem sn sz) cells (x, y) =
+          let
+            xMax = (x + sz - 1)
+            yMax = (y + sz - 1)
+            xRange = [x..xMax]
+            yRange = [y..(yMax - 1)] -- don't double count the corner
+            forwardEdge = (xMax,) <$> yRange
+            bottomEdge = (, yMax) <$> xRange
+          in
+            sum $
+            snd <$>
+            gridPower (Problem sn 1) cells <$>
+            (forwardEdge ++ bottomEdge)
 
-field :: [Point]
-field = (,) <$> [1..297] <*> [1..297]
+recurBox :: Problem -> Cells -> Point -> Power
+recurBox (Problem sn sz) cells (x, y) =
+    let
+      delta = div sz 2
+      corners = [(x, y), (x + delta, y), (x, y + delta), (x + delta, y + delta)]
+    in
+      sum $ snd . (gridPower (Problem sn delta) cells) <$> corners
 
-powerInEachGrid :: SerialNo -> Cells -> [(Corner, [Point])] -> (Cells, [(Corner, Power)])
-powerInEachGrid sn cells gs = foldr (forEachGrid sn) (cells, []) gs
-  where forEachGrid :: SerialNo ->
-                       (Corner, [Point]) ->
-                       (Cells, [(Corner, Power)]) ->
-                       (Cells, [(Corner, Power)])
-        forEachGrid sn (p, g) (cells, rs) = (:rs) . (,) p <$> gridPower sn cells g
+corners :: Size -> [Point]
+corners sz =
+    let maxBound = 300 - sz + 1
+    in (,) <$> [1..maxBound] <*> [1..maxBound]
 
-powerInGrids :: SerialNo -> Size -> Cells -> [Point] -> (Cells, [(Corner, Power)])
-powerInGrids sn size cells corners = powerInEachGrid sn cells $ (id &&& grid size) <$> corners
+sizes :: [Size]
+sizes = [1..300]
 
-maxPowerGrid :: SerialNo -> Size -> Cells -> (Cells, (Corner, Power))
-maxPowerGrid sn size cells = maximumBy powerLevel <$> powerInGrids sn size cells field
-  where powerLevel (_, a) (_, b) = compare a b
+day11 :: SerialNo -> Point
+day11 sn = corner . snd $ problemMax (Problem sn 3) Map.empty
 
-day11 :: SerialNo -> Corner
-day11 sn = fst . snd $ maxPowerGrid sn 3 Map.empty
+day11p2 :: SerialNo -> (Size, Point)
+day11p2 sn =
+    let
+      problems = Problem sn <$> sizes
+      initial = problemMax (head problems) Map.empty
+    in
+      (gridSize &&& corner) $ snd $ foldl' maxPower initial (tail problems)
+  where maxPower :: (Cells, GridResult) -> Problem -> (Cells, GridResult)
+        maxPower (cells, gr) pb =
+          let
+            (cells', gr') = problemMax pb cells
+          in
+            (cells', max gr gr')
+
+problemMax :: Problem -> Cells -> (Cells, GridResult)
+problemMax pb cells =
+    let
+      cs = corners (size pb)
+      initial = GridResult (head cs) (size pb) <$>
+                gridPower pb cells (head cs)
+    in
+      foldl' (maxPower pb) initial (tail cs)
+  where maxPower :: Problem -> (Cells, GridResult) -> Point -> (Cells, GridResult)
+        maxPower pb (cells, gr) pt =
+          let
+            (cells', power) = gridPower pb cells pt
+          in
+            (cells', max gr (GridResult pt (size pb) power))
--- test/Day11Spec.hs
+++ test/Day11Spec.hs
@@ -9,27 +9,36 @@ import Day11
 
 spec :: Spec
 spec = do
-    describe "generate 3x3 points" $ do
-      it "generates a list of points that's a 3x3 grid from the top left" $ do
-        grid 3 (5, 8) `shouldBe`
-          [ (5, 8), (5, 9), (5, 10)
-          , (6, 8), (6, 9), (6, 10)
-          , (7, 8), (7, 9), (7, 10)
-          ]
-
-    describe "powerRemaining" $ do
-      it "returns the formula specified in the problem and caches it" $ do
-        let serialNumber = 8
+    describe "calculating power of a grid" $ do
+      it "runs the calculation for a 1x1 grid" $ do
         let point = (3, 5)
-        powerRemaining serialNumber Map.empty point `shouldBe` ( Map.singleton (3, 5) 4
-                                                               , 4
-                                                               )
-    describe "gridPower" $ do
-      it "calculates the total power for a given grid" $ do
-        let serialNo = 18
-        let corner = (33, 45)
-        let g = grid 3 corner
-        snd (gridPower serialNo Map.empty g) `shouldBe` 29
+        let problem = Problem 8 1
+        gridPower problem Map.empty point
+          `shouldBe` ( Map.singleton (1, point) 4
+                     , 4
+                     )
+      it "adds the power of the lower-sized grid to the leading edge" $ do
+        let cells = Map.insert (2, (1, 1)) 4 $
+                    Map.insert (2, (1, 2)) 3 $
+                    Map.insert (2, (2, 1)) (-1) $
+                    Map.insert (2, (2, 2)) 1 $
+                    Map.insert (1, (1, 1)) 1 $
+                    Map.insert (1, (1, 2)) 3 $
+                    Map.insert (1, (1, 3)) 8 $
+                    Map.insert (1, (2, 1)) 2 $
+                    Map.insert (1, (2, 2)) (-5) $
+                    Map.insert (1, (2, 3)) 2 $
+                    Map.insert (1, (3, 1)) 8 $
+                    Map.insert (1, (3, 2)) 4 $
+                    Map.insert (1, (3, 3)) 9 Map.empty
+        let problem = Problem 1 3
+        let point = (1, 1)
+        gridPower problem cells point
+          `shouldBe` ( Map.insert (3, (1, 1))
+                                  (4 + 8 + 2 + 9 + 8 + 4)
+                                  cells
+                      , (4 + 8 + 2 + 9 + 8 + 4)
+                     )
     describe "day11" $ do
       it "returns the (x,y) coordinates of the top left corner of the grid with the highest power" $ do
         day11 18 `shouldBe` (33, 45)
