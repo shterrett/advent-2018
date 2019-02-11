412edea1c0da1111605f31e9f88df42853045de7
Stuart Terrett <shterrett@gmail.com>
2019-02-11 07:52:46 -0500

    WIP

--- src/Day12.hs
+++ src/Day12.hs
@@ -7,10 +7,13 @@ import Data.Tuple (swap)
 import qualified Data.Text as T
 import qualified Data.HashMap.Strict as Map
 import Data.HashMap.Strict (HashMap)
+import Data.Maybe (fromMaybe)
 import Text.Parsec
 import Text.Parsec.Char (anyChar, space)
 
+type Pot = Char
 type Pots = String
+type Window = Pots
 type Rules = HashMap String Char
 
 parseInput :: [T.Text] -> Either ParseError (Rules, Pots)
@@ -24,3 +27,33 @@ parsePots = drop 15
 ruleParser :: Parsec String () (String, Char)
 ruleParser = (,) <$> manyTill pot space <*> (string "=> " >> pot)
   where pot = oneOf ".#"
+
+windows :: Int -> [a] -> [[a]]
+windows n ls | length ls < n = []
+             | otherwise = (take n ls):(windows n $ drop 1 ls)
+
+stepPot :: Rules -> Window -> Pot
+stepPot rules = fromMaybe '.' . (flip Map.lookup) rules
+
+nextGen :: Rules -> [Window] -> Pots
+nextGen rules ws = fmap (stepPot rules) ws
+
+augmentPots :: Pots -> Pots
+augmentPots ps = ".." ++ ps ++ ".."
+
+simulate :: Int -> (Rules, Pots) -> Pots
+simulate = undefined
+
+sumPlantBearing :: Pots -> Int
+sumPlantBearing ps =
+    sum $ fmap snd $ filter inBase $ filter hasPlant $ zip ps [-2, -1..]
+  where hasPlant ('#', _) = True
+        hasPlant _ = False
+        inBase (_, x) = 0 <= x && x <= 25
+
+day12 :: [T.Text] -> T.Text
+day12 input =  either (T.pack . show) (T.pack . show) $
+               sumPlantBearing .
+               simulate 20 .
+               (fmap augmentPots) <$>
+               parseInput input
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -3,6 +3,8 @@
 module Day12Spec where
 
 import Test.Hspec
+import qualified Data.HashMap.Strict as Map
+import Data.HashMap.Strict (HashMap)
 import Day12
 
 spec :: Spec
@@ -17,4 +19,41 @@ spec = do
                     , ".#.#. => #"
                     ]
         snd <$> parseInput input `shouldBe` (Right "#..#.#..##......###...###")
+    describe "windows" $ do
+      it "returns a list of windows of the given length with step size 1" $ do
+        let l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        windows 3 l `shouldBe` [ [1, 2, 3]
+                               , [2, 3, 4]
+                               , [3, 4, 5]
+                               , [4, 5, 6]
+                               , [5, 6, 7]
+                               , [6, 7, 8]
+                               , [7, 8, 9]
+                               , [8, 9, 10]
+                               ]
+    describe "next generation" $ do
+      let rules = Map.insert "...##" '#' $
+                  Map.insert "..#.." '#' $
+                  Map.insert ".#..." '#' $
+                  Map.insert ".#.#." '#' $
+                  Map.empty
 
+      it "acts on a single list according to the parameters" $ do
+        let l1 = "..#.."
+        let l2 = "#...#"
+        stepPot rules l1 `shouldBe` '#'
+        stepPot rules l2 `shouldBe` '.'
+      it "maps over a list of windows" $ do
+        nextGen rules [ "..#.."
+                      , "#...#"
+                      , ".#..."
+                      , "....."
+                      , ".#.#."
+                      ]
+          `shouldBe` "#.#.#"
+    describe "augment pots" $ do
+      it "adds two pots to the beginning and two to the end os the given pots are always considered" $ do
+        augmentPots "#......#" `shouldBe` "..#......#.."
+    describe "sumPlantBearing" $ do
+      it "returns the sum of the index of the plant-bearing pots, remembering to account for the extra 2 on each end" $ do
+        sumPlantBearing ".#....##....#####...#######....#.#..##." `shouldBe` 325
